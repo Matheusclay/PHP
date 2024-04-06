0361977db9260e0a8b5991714b1f6d36d4ca6f6f
Matheus Henrique da Silva <matheeus.silva9@hotmail.com>
2024-04-05 21:50:44 -0300

    1

---
+++ processar_media.php
@@ -0,0 +1,24 @@
+<?php
+
+    $n1 = $_POST['n1'];
+    $n2 = $_POST['n2'];
+    $n3 = $_POST['n3'];
+    $n4 = $_POST['n4'];
+    $faltas = $_POST['faltas'];
+    $media = ($n1 + $n2 + $n3 + $n4) / 4;
+    if (($media >= 6) && ($faltas <= 10)){
+        echo "Aprovado! ";
+    } else {
+        echo "Reprovado!";
+    }
+    
+    $vetor = array(10, 20, 30);
+    echo $vetor[1];
+    $vetor[3] = 40;
+    echo "<br>" .$vetor[3];
+    $vetor["tio"] = "tio";
+    echo "<br>" .$vetor["tio"];
+
+    foreach ($vetor as $v){
+        echo "$v <br>";
+    }
