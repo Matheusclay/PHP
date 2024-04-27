f501309668e3016ba9ae0bd31ce484380f4f6f98
Matheus Henrique da Silva <matheeus.silva9@hotmail.com>
2024-04-26 21:32:14 -0300

    Update exerc4resposta.php

--- Lista 2/exerc4resposta.php
+++ Lista 2/exerc4resposta.php
@@ -1,17 +1,17 @@
 <?php
 $meses = array(
     1 => "Janeiro",
-    2 => "Fevereiro",
-    3 => "Março",
-    4 => "Abril",
-    5 => "Maio",
-    6 => "Junho",
-    7 => "Julho",
-    8 => "Agosto",
-    9 => "Setembro",
-    10 => "Outubro",
-    11 => "Novembro",
-    12 => "Dezembro"
+        2 => "Fevereiro",
+            3 => "Março",
+                4 => "Abril",
+                    5 => "Maio",
+                        6 => "Junho",
+                            7 => "Julho",
+                                8 => "Agosto",
+                                    9 => "Setembro",
+                                        10 => "Outubro",
+                                            11 => "Novembro",
+                                                12 => "Dezembro"
 );
 
 if(isset($_POST['numero'])){
